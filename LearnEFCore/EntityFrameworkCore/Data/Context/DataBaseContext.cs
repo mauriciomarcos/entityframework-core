c5f831d7406c9f52142f07dc5314c24eb2449eb8
MMarcos <mauricio.marcos@castgroup.com.br>
2020-09-13 22:12:09 -0300

    Adicionada novas dependências do EFCore

---
+++ LearnEFCore/EntityFrameworkCore/Data/Context/DataBaseContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EntityFrameworkCore.Data.Context
+{
+    public class DataBaseContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=blog.db");
+        }
+    }
+}
